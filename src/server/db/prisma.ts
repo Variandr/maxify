ab58932edde3152533026769680dd37f5144b69a
Variandr <boltyn02@gmail.com>
2022-09-20 22:07:39 +0300

    ref: refactor

--- src/server/db/prisma.ts
+++ src/server/db/prisma.ts
@@ -1,20 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
-// Make global.cachedPrisma work with TypeScript
 declare global {
-  // NOTE: This actually needs to be a "var", let/const don't work here.
   // eslint-disable-next-line no-var
   var cachedPrisma: PrismaClient
 }
 
-export type Unsaved<T> = Omit<T, 'id' | 'createdAt' | 'updatedAt'> & {
-  // make timestamps optional
-  createdAt?: Date
-  updatedAt?: Date
-}
-
-// Workaround to make Prisma Client work well during "next dev"
-// @see https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
@@ -26,9 +16,6 @@ if (process.env.NODE_ENV === 'production') {
   prisma = global.cachedPrisma
 }
 
-//
-// We export $queryRaw as a sql method to allow syntax coloring on template strings
-//
 export const sql = prisma.$queryRaw.bind(prisma)
 
 export default prisma
