13d10f5652b67089a5f4e30d4880e325a01397b5
Gepuy <maxbondarev93@gmail.com>
2022-09-30 00:07:04 +0300

    fix: renamed error messages

--- src/client/lib/types/api.ts
+++ src/client/lib/types/api.ts
@@ -1,5 +1,5 @@
 export enum ErrorMessage {
-  YOU_HAVE_INCORRECT_DATA = 'Your data is incorrect',
-  ACCOUNT_WAS_NOT_FOUND = 'Your account wasn`t found',
+  YOU_HAVE_INCORRECT_DATA = 'Please enter correct data',
+  ACCOUNT_WAS_NOT_FOUND = 'User with that email was not found',
   INCORRECT_PASSWORD = 'You`ve got incorrect password',
 }
